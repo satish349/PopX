091a13df29babe0ee14e7e6c08c3b8f0ac6eab2a
Satish Kumar <satishkumar848210@gmail.com>
2025-08-19 15:42:44 +0530

    Fix LandingPage imports and routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./landing.jsx";
+import LandingPage from "./Landing.jsx";
 import RegisterPage from "./register";
 import LoginPage from "./login";
 import ProfilePage from "./Profile";
