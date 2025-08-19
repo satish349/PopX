9e9249a0a0e2fc48e93b8d8ff2ade3244f0ced8d
Satish Kumar <satishkumar848210@gmail.com>
2025-08-19 14:19:40 +0530

    Fix landing import path

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './index.css'
-import LandingPage from './landing'
 import RegisterPage from './register'
 import LoginPage from './login'
 import ProfilePage from './Profile'
@@ -12,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element={<LandingPage/>} />
         <Route path = "/register" element={<RegisterPage/>}/>
         <Route path = '/login' element={<LoginPage/>} />
         <Route path = "/profile" element={<ProfilePage/>}/>
